63640b64ad3417ae1d3615783b7ab5fff425c116
sonwon9 <66217099+sonwon9@users.noreply.github.com>
2020-09-28 01:04:36 +0900

    Add Homework Code(Week 3-2, Challenge)

---
+++ 06_challenge_1.ino
@@ -0,0 +1,48 @@
+#define PIN_LED 7
+
+int p; // p : period ( 100 to 10000, unit : us), 1 ms = 1000 us
+
+void set_period(int period) {
+  p = period;
+}
+
+void set_duty(int duty) { // duty : 0 to 100 (unit : %)
+  int new_p = p / 100;
+  digitalWrite(PIN_LED, 0);
+  delayMicroseconds(p * duty);
+  digitalWrite(PIN_LED, 1);
+  delayMicroseconds(new_p * (100 - duty));
+}
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+  int running_time = 1000;
+
+  //set_period(100);
+  //set_period(1000);
+  +set_period(10000);
+
+  int d = 0;
+  int i = 1;
+  unsigned long ts = millis();
+
+  if (p == 10000) {
+    running_time = 2000;
+  }
+
+  while (millis() - ts <= running_time) {
+    set_duty(d);
+    d += i;
+    if (d <= 0 or d >= 100) {
+      i = -i;
+    }
+  }
+  digitalWrite(PIN_LED, 1);
+  while (1);
+
+
+}
